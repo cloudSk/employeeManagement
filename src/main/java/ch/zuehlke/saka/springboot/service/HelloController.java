0e5866036705beaff442ac6593daf37b86abdfdd
Sascha Kaufmann <sascha.kaufmann92@gmail.com>
2018-09-17 10:32:13 +0200

    add HelloController and integration test

---
+++ src/main/java/ch/zuehlke/saka/springboot/service/HelloController.java
@@ -0,0 +1,13 @@
+package ch.zuehlke.saka.springboot.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String getHello() {
+		return "Hello dear Sir.";
+	}
+}
---
+++ src/test/java/ch/zuehlke/saka/springboot/service/HelloControllerIntegrationTest.java
@@ -0,0 +1,41 @@
+package ch.zuehlke.saka.springboot.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerIntegrationTest {
+	@LocalServerPort
+	private int port;
+
+	private URI base;
+
+	@Autowired
+	private TestRestTemplate template;
+
+	@Before
+	public void setUp() {
+		base = URI.create("http://localhost:" + port + "/hello");
+	}
+
+	@Test
+	public void getHello() {
+		ResponseEntity<String> response = template.getForEntity(base, String.class);
+
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.OK);
+		assertThat(response.getBody()).isEqualTo("Hello dear Sir.");
+	}
+}
